d757181ffe50b742c07e6677450d3a0afe5f88a4
root <root@localhost.localdomain>
2026-09-05 18:46:46 +0800

    docs: prepare v0.10.24 release metadata

--- tests/test_community_health.py
+++ tests/test_community_health.py
@@ -31,7 +31,7 @@ class CommunityHealthTests(unittest.TestCase):
             "authors:",
             'repository-code: "https://github.com/sandbaseai/awesome-workbuddy"',
             'license: CC0-1.0',
-            "version: 0.10.23",
+            "version: 0.10.24",
             "date-released: 2026-09-05",
         ):
             self.assertIn(field, citation)
